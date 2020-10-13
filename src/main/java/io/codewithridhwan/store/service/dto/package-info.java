93d758b8f2c926fc52ec043cf5c992fb93f12cec
Ridhwan Oladejo <ridhwan.oladejo@9mobile.com.ng>
2020-10-13 11:21:55 +0100

    Initial version of store generated by JHipster-6.10.1

---
+++ src/main/java/io/codewithridhwan/store/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package io.codewithridhwan.store.service.dto;
---
+++ src/main/java/io/codewithridhwan/store/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MapStruct mappers for mapping domain objects and Data Transfer Objects.
+ */
+package io.codewithridhwan.store.service.mapper;
